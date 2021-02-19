724dc22c3229dfe2fce03278b1bf2a6d5952644f
Jim Geist <info@jimgeist.com>
2021-02-19 16:19:13 -0500

    v2.00: Final version of flask Customer Satisfation Survey. Survey server code utilizes session storage to control survey flow and a cookie to remember the survey between browser sessions.

--- README.md
+++ README.md
@@ -9,11 +9,11 @@ Build a survey application that asks the visitor to answer questions. The visito
 ### SURVEY FLOW ###
 The site root page, **/** serves as the Welcome page. The only survey available for now is the Customer Satisfaction Survey. The survey name, Customer Satisfaction Survey, appears on the page as well as the page title. The directions for completing the Customer Satisfaction Survey are from the survey declaration in surveys.py. Pressing Start makes a POST call to **/session** which sets up session storage and depending on the number of answers in session storage, either serves up the appropriate question or renders the thank you page.
 
-The **/questions** route displays the survey question and the choices to the visitor. The question number and total number of questions appear with the question so the visitor can guage their progress. The route does NOT include the question number, that is, you cannot change the route to /question/4 to get to the fourth question. "Answering the question should fire off a POST request to /answer with the answer the user selected" was taken quite literally and there is no Next button. A click on an answer advances to the next question (actually to the answer processor which redirects to the next question).
+The **/questions** route displays the survey question and the choices to the visitor. The question number and total number of questions appear with the question so the visitor can guage their progress. The route does NOT include the question number, that is, you cannot change the route to /question/4 to get to the fourth question. "Answering the question should fire off a POST request to /answer with the answer the user selected" was taken quite literally and there is no 'Next' button. A click on an answer advances to an answer processor which either redirects back to **/questions** or to **/thankyou** when all questions were answered. A redirect to the **/thankyou** route does exist in the **/questions** route to catch the case where the survey is completed, but the visitor changes the url to /questions
 
-When the final question is answered, the visitor is receives a thank you page accomplished by a redirect that exists on the question page. The thank you page includes the questions and responses provided by the visitor. The questions and answers are filtered through **|safe** to ensure that embedded html elements are not changed to text.
+The **/thankyou** route thanks the vistor for taking the survey. The answer processor redirects to the thank you page when all survey questions were answered. The thank you page includes the questions and responses provided by the visitor. The questions and answers are filtered through **|safe** to ensure that embedded html elements are not changed to text.
 
-The **/thankyou** route includes logic to ensure the survey was completed (number of responses equals total number of questions) before displaying the thank you message. The visitor is automatically redirected to the welcome page to restart the survey when the checks indicate an incomplete survey (example - changing /questions to /thankyou when questions still remain). 
+The **/thankyou** route includes logic to ensure the survey was completed (number of responses equals total number of questions) before displaying the thank you message. The visitor is automatically redirected the questions page when the check indicates that all questions were not answered (example - changing /questions to /thankyou when questions still remain). 
 
 Debugging tool declarations remain in app.py and are commented out. A **/reset** route was added to provides a means of resetting the survey control structures without having to recycle the server. 
 
@@ -23,14 +23,20 @@ Movement through the survey is controlled the survey answers that are saved in t
 ### ENHANCEMENTS ###
 - Current question number and total number of questions appear on each question.
 - Light JavaScript added to submit the answer when the visitor clicks on an answer.
+- We are in the midst of a pandamnit and toilet paper, not frisbees is a particulary hottly hoarded commodity (at least it was when the pandamnit started).
 - Thank You page includes the questions and the answers provided by the site visitor.
-- **/reset** route to reset the survey control variables. 
+- **/reset** route to reset the session and cookie storage. 
+- Survey uses session storage to hold the question responses and the flag that controls the visibility, and a cookie that holds the responses, last active date, and survey title.
+- Helpful messaging appears on the welcome page that informs the visitor if they completed the survey or if the survey was started but never completed. The button on the welcome page coupled with the message lets the visitor know how the survey will flow.
+- Debugging capabilities while survey is in progress. This was where I should have stopped . . but didn't!
+  - adding, **?debug** on the welcome page sets debugging to true for the entire survey session. The cookie and session storage values are added to the bottom of each survey page. 
+  - when debugging is not currently active, the ability to add cookie and session storage values to the bottom of the a non-welcome is possible again via **?debug**. The cookie and session details are added just for the active page (**?debug** on the welcome page turns debugging on for survey session as described above).
 
 
 ### DIFFICULTIES ###
-Not sure why the assignment suggesting changing the Start button to a POST request. Is it because we are establishing / altering the session storage.
+Got lost overdoing things, making changes, then going back. The debugging pieces consumed a lot of time getting them to work correctly and retrofitting was required since a cookie was used to determine the starting point.
 
 
 ### TIMING ###
-- 4.3 hours (high end for assignment was 8 hours). 
+- 4.3 hours (high end for assignment was 8 hours). And then I ruined it by adding the cookie and destroyed the timeline with debugging statements. Total time with cookie and debugging pieces brings this one in at 14 hours {sigh}.
 
--- app.py
+++ app.py
@@ -17,46 +17,75 @@ app.config['SECRET_KEY'] = "the password is 'password'"
 #  to each question as the respondent moved through the survey.
 SURVEY_SESSION = "survey_session"
 
+# survey_data = {
+#     SURVEY_SESSION_RESPONSES: [],
+#     SURVEY_SESSION_DEBUG: True or False
+# }
+# Where SURVEY_SESSION_RESPONSES = "responses" and SURVEY_SESSION_DEBUG = "show_debug_info"
+SURVEY_SESSION_RESPONSES = "responses"
+SURVEY_SESSION_DEBUG = "show_debug_info"
+
+
 # Cookie Constants
 COOKIE_NAME = "Survey_CustSat"
 COOKIE_DELIM = "<!>"
 COOKIE_EXPIRY = 5184000  # 5,184,000 seconds (60 days) = cookie expiration date
 
-# Debug session controls whether session details (the survey answer storage) appear on
-#  the bottom of each page.
-# Do not remove DEBUG_DISPLAY_SESSION. Just set it to False when no longer needed.
-DEBUG_DISPLAY_SESSION = True
 
 # survey title
 title = satisfaction_survey.title
 
 
-def bake_cookie(title, is_survey_complete):
-    """ Creates a cookie for the survey with the latest responses.
-        The cookie data consists of the survey responses, the current date,
-        and whether the survey is complete. The list is converted to a string
-        with <!> separating each field.
+def assemble_session_data(survey_title):
+    """ Assembles session data into an object,
+        assembled_data = {
+                cookie_data
+                show_debug_info
+            }
+        cookie_data is a sting with the survey responses, last activity date,
+        and the survey title.
+        show_debug_info is a True/False for session debugging.
+
+        The show_debug_info controls whether debugging data -- the data in
+        the cookie and the data in session storate -- are visible on the
+        bottom of the page. The show_debug_info is stored in the session,
+        session_data = {
+            responses
+            show_debug_info
+        }
+        and since we are reading the session data for the responses, we
+        should also get the debug flag so we do not perform an additional
+        read of session data when we can get the debug flag in this
+        function.
+
+        The cookie data consists of the survey responses, the current date, and
+        the survey's title. The list is converted to a string with <!> separating
+        each field.
+
         When using this cookie, the string is converted to a list by splitting
-        it at <!>, the True/False text is popped off, followed by the date
-        string. The remaining bit is a string with the responses.
+        it at <!>, the survey title text is popped off, followed by the date
+        string. The remaining data are the responses to the survey questions.
     """
 
-    responses = list(session[SURVEY_SESSION])
+    session_data = session[SURVEY_SESSION]
+    responses = list(session_data.get(SURVEY_SESSION_RESPONSES, []))
     responses.append(str(datetime.now()))
-    responses.append(str(is_survey_complete))
+    responses.append(survey_title)
 
-    return (COOKIE_DELIM).join(responses)
+    return {
+        "cookie_data": (COOKIE_DELIM).join(responses),
+        SURVEY_SESSION_DEBUG: session_data.get(SURVEY_SESSION_DEBUG, False)
+    }
 
 
 def get_cookie_data():
     """ function reads the cookie COOKIE_NAME and returns the following object
         with the data from the cookie:
         {
-            cookie_data: cookie data string with all fields concatenated together.
+            cookie_data: raw cookie data -- string of all the fields concatenated together.
             responses: [list containing the answers to each survey question]
             date_last_activity: date of last activity in 2021-02-17 22:53:57.423475 format
-            completed: True / False indicator as to whether the survey was completed.
-                        completed indicator is not used in code at this time.
+            title: the title of the survey.
 
         }
 
@@ -69,26 +98,23 @@ def get_cookie_data():
         # we have cookie data
         try:
             responses = cookie_data.split(COOKIE_DELIM)
-            cookie_data_out["completed"] = bool(responses.pop())
+            cookie_data_out["title"] = responses.pop()
             cookie_data_out["date_last_activity"] = responses.pop()
             date_obj = datetime.strptime(
                 cookie_data_out["date_last_activity"], "%Y-%m-%d %H:%M:%S.%f")
             cookie_data_out["responses"] = responses
         except:
             # Most likely a ValueError
-            # debug needs a value because it controls whether session and cookie data appears on the
-            #  page for debugging purposes
             cookie_data_out["responses"] = []
     else:
         cookie_data_out["responses"] = []
-    print(
-        f"get_cookie_data: cookie_data={cookie_data}\n\ncookie_data_out={cookie_data_out}", flush=True)
+
     return cookie_data_out
 
 
-def determine_start_via_cookie():
+def determine_start_via_cookie(show_debug_info):
     """ read the "Survey_CustSat cookie. The cookie is broken down into the
-        previous responses, last activity date, and completion flag when
+        previous responses, last activity date, and survey title when
         cookie data exists.
     """
 
@@ -96,19 +122,20 @@ def determine_start_via_cookie():
     starting_info = {"cookie_data": cookie_data["cookie_data_string"]}
 
     # look at the data and determine messaging.
-    # 1 - 3 responses: Survey was started on dddd. Let's resume where we left off.
-    #   (button is resume Survey)
-    # 4 responses: Survey was completed on dddd. Click View Results to see your responses.
+    # 1 - 3 responses: Survey was started on {date}. Let's resume where we left off.
+    #   (button is Resume Survey)
+    # 4 responses: Survey was completed on {date}. Click View Results to see your responses.
     #   (button is View Results)
-
-    print(
-        f"determine_start_via_cookie: cookie_data: {cookie_data}, \n ", flush=True)
+    # 0 responses: no message, button is Start Survey.
 
     if (len(cookie_data.get("responses", [])) > 0):
 
-        session[SURVEY_SESSION] = cookie_data["responses"]
-        # date_obj = datetime.strptime("2021-02-17 22:53:57.423475",
-        #     "%Y-%m-%d %H:%M:%S.%f")
+        session_data = {
+            SURVEY_SESSION_RESPONSES: cookie_data["responses"],
+            SURVEY_SESSION_DEBUG: show_debug_info
+        }
+
+        # date_obj = datetime.strptime("2021-02-17 22:53:57.423475", "%Y-%m-%d %H:%M:%S.%f")
         # datetime_object.strftime("%B %d, %Y at %I:%M %p")
         date_obj = datetime.strptime(
             cookie_data["date_last_activity"], "%Y-%m-%d %H:%M:%S.%f")
@@ -118,58 +145,29 @@ def determine_start_via_cookie():
             starting_info["button_text"] = "View Results"
             starting_info["has_message"] = True
         else:
-            starting_info["message"] = f"Survey was started on {date_str}. Let's resume where we left off. "
+            starting_info["message"] = f"Survey was started on {date_str}. Let's start from where we left off. "
             starting_info["button_text"] = "Resume Survey"
             starting_info["has_message"] = True
 
     else:
         starting_info["has_message"] = False
         starting_info["button_text"] = "Start Survey"
+        session_data = {
+            SURVEY_SESSION_RESPONSES: [],
+            SURVEY_SESSION_DEBUG: show_debug_info
+        }
+
+    session[SURVEY_SESSION] = session_data
 
     return starting_info
-
-    # cookie_data = request.cookies.get(COOKIE_NAME, "")
-    # starting_info = {"cookie_data": cookie_data}
-
-    # if (len(cookie_data) > 0):
-    #     # we have cookie data
-    #     responses = cookie_data.split(COOKIE_DELIM)
-    #     completed = responses.pop()
-    #     date_last_activity = responses.pop()
-    #     session[SURVEY_SESSION] = responses
-
-    #     # look at the data and determine messaging.
-    #     # 1 - 3 responses: Survey was started on dddd. Let's resume where we left off.
-    #     #   (button is resume Survey)
-    #     # 4 responses: Survey was completed on dddd. Click View Results to see your responses.
-    #     #   (button is View Results)
-    #     if (len(responses) > 0):
-    #         # date_obj = datetime.strptime("2021-02-17 22:53:57.423475",
-    #         #     "%Y-%m-%d %H:%M:%S.%f")
-    #         # datetime_object.strftime("%B %d, %Y at %I:%M %p")
-    #         date_obj = datetime.strptime(
-    #             date_last_activity, "%Y-%m-%d %H:%M:%S.%f")
-    #         date_str = date_obj.strftime("%B %d, %Y at %I:%M %p")
-    #         if (len(responses) == 4):
-    #             starting_info["message"] = f"Survey was completed on {date_str}. Click 'View Results' to see your responses. "
-    #             starting_info["button_text"] = "View Results"
-    #             starting_info["has_message"] = True
-    #         else:
-    #             starting_info["message"] = f"Survey was started on {date_str}. Let's resume where we left off. "
-    #             starting_info["button_text"] = "Resume Survey"
-    #             starting_info["has_message"] = True
-
-    # else:
-    #     starting_info["has_message"] = False
-    #     starting_info["button_text"] = "Start Survey"
-
-    # return starting_info
 
 
 def get_question_idx():
     """ get the index for the next question based on number of saved responses """
 
-    return len(session[SURVEY_SESSION])
+    session_data = session[SURVEY_SESSION]
+
+    return len(session_data.get(SURVEY_SESSION_RESPONSES, []))
 
 
 @ app.route("/")
@@ -181,17 +179,17 @@ def survey_welcome():
         responses structure in session storage. The /session route then calls
         /questions
 
+        ?debug adds cookie and session data to the bottom of the page and sets
+        the debug flag to True for the duration of the survey/session.
     """
 
     show_debug_info = request.args.get("debug", False)
     show_debug_info = True if (show_debug_info == "") else False
 
-    print(f"show_debug_info: {show_debug_info}", flush=True)
-
     instructions = satisfaction_survey.instructions
 
     # retrieve the cookie, if any
-    start_info = determine_start_via_cookie()
+    start_info = determine_start_via_cookie(show_debug_info)
     if start_info.get("has_message", False):
         flash(start_info["message"], "okay")
 
@@ -216,17 +214,18 @@ def session_setup():
     """
 
     try:
-        responses = session[SURVEY_SESSION]
+        session_data = session[SURVEY_SESSION]
+        responses = session_data[SURVEY_SESSION_RESPONSES]
 
     except KeyError:
         # session does not exist for SURVEY_SESSION
         responses = []
-        session[SURVEY_SESSION] = responses
+        session_data = {SURVEY_SESSION_RESPONSES: responses}
+        session[SURVEY_SESSION] = session_data
 
     # the number of responses in session must be less than the number
     #  of questions in order to ask a question. Otherwise, the survey has
     #  already ended.
-    print(f"/session: responses={responses}", flush=True)
     if (len(responses) < len(satisfaction_survey.questions)):
         return redirect("/questions")
     else:
@@ -244,19 +243,23 @@ def survey_questions():
         asked and answered, next question to ask is idx 2 (since questions
         at idx 0 and idx 1 were already asked).
 
-        The page presents the current survey question and possible
-        answer choices as radio buttons.
+        The page presents the current survey question and possible answer choices 
+        as radio buttons.
 
-        Answering the question (no button) generates a post
-        request to /answer with the answer selected.
+        Answering the question (no button) generates a post request to /answer 
+        with the answer selected.
 
-        Answer page will eventually redirect back to questions
-        where the next question is asked.
+        Answer page will eventually redirect back to questions where the next 
+        question is asked.
+
+        ?debug adds the cookie and session details to the bottom of the page. The 
+        debug flag persists only for the current page. Add ?debug while on the welcome 
+        page when session and cookie details are desired for the entire session.
 
     """
 
     show_debug_info = request.args.get("debug", False)
-    show_debug_info = True if (show_debug_info == "on") else False
+    show_debug_info = True if (show_debug_info == "") else False
 
     # The survey question number to ask is determined by the number of questions
     #  answered. Function get_question_idx() returns the index of the question to
@@ -283,17 +286,16 @@ def survey_questions():
             answer, f"{idx}_{answer.replace(' ', '-')}"))
         idx = idx + 1
 
-    print(
-        f"/questions: session={session[SURVEY_SESSION]}", flush=True)
-    cookie_data = bake_cookie(title, False)
-
-    print(
-        f"/questions: cookie_data={cookie_data},\nsession={session[SURVEY_SESSION]}", flush=True)
+    session_info = assemble_session_data(title)
+    # Was debugging overriden for just this page?
+    if ((session_info.get(SURVEY_SESSION_DEBUG, False)) or (show_debug_info)):
+        show_debug_info = True
+    else:
+        show_debug_info = False
 
     # render the questions page. Note that within questions.html, the question number
     #  presented to the respondent is question_nbr + 1. The respondent sees
     #  1 as the first question, not 0.
-
     html = render_template("questions.html", survey_title=title,
                            question_nbr=question_nbr,
                            question_nbr_max=(
@@ -302,22 +304,12 @@ def survey_questions():
                            question_answers=answers,
                            debug=show_debug_info,
                            session_name=SURVEY_SESSION,
-                           cookie=cookie_data)
+                           cookie=session_info["cookie_data"])
 
     resp_obj = make_response(html)
-    resp_obj.set_cookie(COOKIE_NAME, cookie_data, COOKIE_EXPIRY)
-    resp_obj.set_cookie("Survey_Flag", "True", COOKIE_EXPIRY)
+    resp_obj.set_cookie(
+        COOKIE_NAME, session_info["cookie_data"], COOKIE_EXPIRY)
     return resp_obj
-
-    # return render_template("questions.html", survey_title=title,
-    #                        question_nbr=question_nbr,
-    #                        question_nbr_max=(
-    #                            len(satisfaction_survey.questions)),
-    #                        question_text=question_text,
-    #                        question_answers=answers,
-    #                        debug=DEBUG_DISPLAY_SESSION,
-    #                        session_name=SURVEY_SESSION,
-    #                        cookie=cookie_data)
 
 
 @ app.route("/answer", methods=["POST"])
@@ -329,13 +321,14 @@ def survey_answer():
     # We need to get their answer in order to save it.
     # radio button choices are named q-#-choices where # is the question
     #  number.
-    responses = session[SURVEY_SESSION]
-    question_nbr = len(responses)
+    session_data = session[SURVEY_SESSION]
+    responses = session_data.get(SURVEY_SESSION_RESPONSES, [])
+    question_nbr = len(session_data.get(SURVEY_SESSION_RESPONSES, []))
 
     answer = request.form[f'q-{question_nbr}-choices']
 
-    responses.append(answer)
-    session[SURVEY_SESSION] = responses
+    session_data[SURVEY_SESSION_RESPONSES].append(answer)
+    session[SURVEY_SESSION] = session_data
 
     # Is there another question? Check the number of responses to find out.
     if (len(responses) < len(satisfaction_survey.questions)):
@@ -346,10 +339,20 @@ def survey_answer():
 
 @ app.route("/thankyou")
 def survey_thankyou():
-    """ Handles the thank you page for the survey. """
+    """ Handles the thank you page for the survey. 
+
+        ?debug adds the cookie and session details to the bottom of the page. The 
+        debug flag persists only for the current page. Add ?debug while on the welcome 
+        page when session and cookie details are desired for the entire session.
+
+    """
+
+    show_debug_info = request.args.get("debug", False)
+    show_debug_info = True if (show_debug_info == "") else False
 
     # Is this legitimate? Was the survey completed?
-    responses = session[SURVEY_SESSION]
+    session_data = session[SURVEY_SESSION]
+    responses = session_data.get(SURVEY_SESSION_RESPONSES, [])
     if (len(responses) == len(satisfaction_survey.questions)):
         questions_answers = "Your responses:<br>"
         idx = 0
@@ -357,42 +360,47 @@ def survey_thankyou():
             questions_answers = f"{questions_answers}{idx + 1}. {question.question}  <strong>{responses[idx]}</strong><br><br>"
             idx = idx + 1
 
-        cookie_data = bake_cookie(title, True)
+        session_info = assemble_session_data(title)
+        # Was debugging overriden for just this page?
+        if ((session_info.get(SURVEY_SESSION_DEBUG, False)) or (show_debug_info)):
+            show_debug_info = True
+        else:
+            show_debug_info = False
 
         html = render_template("thank_you.html", survey_title=title,
                                q_and_a=questions_answers,
-                               debug=DEBUG_DISPLAY_SESSION,
+                               debug=show_debug_info,
                                session_name=SURVEY_SESSION,
-                               cookie=cookie_data)
+                               cookie=session_info["cookie_data"])
 
         resp_obj = make_response(html)
-        resp_obj.set_cookie(COOKIE_NAME, cookie_data, COOKIE_EXPIRY)
+        resp_obj.set_cookie(
+            COOKIE_NAME, session_info["cookie_data"], COOKIE_EXPIRY)
         return resp_obj
 
-        # return render_template("thank_you.html", survey_title=title,
-        #                        q_and_a=questions_answers,
-        #                        debug=DEBUG_DISPLAY_SESSION,
-        #                        session_name=SURVEY_SESSION,
-        #                        cookie=cookie_data)
     else:
         # restart the survey
         # A lot can happen here. The respondent can also get reset to the next natural question. But for now,
         #  reset the survey to demonstrate the use of a flash message.
-        session[SURVEY_SESSION] = []
-        flash("Some survey shenanigans were detected. Your survey was reset.", "warning")
-        return redirect("/")
+
+        flash("Some survey shenanigans were detected &#x1F609;!", "warning")
+        return redirect("/questions")
 
 
 @ app.route("/reset")
 def survey_reset():
-    """ function to reset the survey so there is no need to stop the server for testing """
+    """ function to reset the survey so there is no need to stop the server for testing 
 
-    session[SURVEY_SESSION] = []
+        The thank you page template is displayed since a response dialog is needed to clear
+        the cookie. The page includes a link back to the start page for the survey. 
+    """
 
+    session[SURVEY_SESSION][SURVEY_SESSION_RESPONSES] = []
+    session_reset_info = f'SURVEY WAS RESET<br><br><a href="/">{title} welcome page</a>'
     # clear the cookie
     html = render_template("thank_you.html", survey_title=title,
-                           q_and_a="",
-                           debug=False,
+                           q_and_a=session_reset_info,
+                           debug=True,
                            session_name=SURVEY_SESSION,
                            cookie="")
 
